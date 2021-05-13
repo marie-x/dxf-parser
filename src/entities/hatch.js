a86e7cc466dbaa1d36b11051d33c623cb7b65b22
Ken Riley <kenr@nodots.com>
2021-05-12 19:22:10 -0500

    Basic HATCH stuff blocked out. Now need to deal with sub-entities

--- src/entities/hatch.js
+++ src/entities/hatch.js
@@ -1,4 +1,3 @@
-
 import * as helpers from '../ParseHelpers'
 
 export default function EntityParser() { }
@@ -12,6 +11,9 @@ var BoundaryPathTypes = {
     4: 'derived',
     8: 'textbox',
     16: 'outermost',
+    6: 'polyline derived',
+    7: 'external polyline derived',
+    22: 'polyline derived outermost'
 };
 
 var EdgeTypes = {
@@ -22,12 +24,10 @@ var EdgeTypes = {
 }
 
 EntityParser.prototype.parseEntity = function (scanner, curr) {
-    console.log('EntityParser.prototype.parseEntity HATCH')
-    // console.log(curr)
     var entity = {
         type: curr.value,
         elevationPoint: { x: null, y: null, z: null },
-        extrusionDirection: { x: null, y: null, z: null }
+        extrusionDirection: { x: null, y: null, z: null },
     };
 
     curr = scanner.next();
@@ -35,103 +35,132 @@ EntityParser.prototype.parseEntity = function (scanner, curr) {
         if (curr.code === 0) break;
 
         switch (curr.code) {
-            // this is eating subsequent 10s and 20s
-            // case 10:
-            //     entity.elevationPoint.x = curr.value;
-            //     break;
-            // case 20:
-            //     entity.elevationPoint.y = curr.value;
-            //     break;
-            // case 10:
-            //     entity.elevationPoint.z = curr.value;
-            //     break;
+            case 5:
+                break;
+            case 330:
+                break;
+            case 100:
+                // subclass marker
+                break;
+            case 10:
+                // elevation point x
+                entity.elevationPoint.x = curr.value;
+                break;
+            case 20:
+                // y value of elevation
+                entity.elevationPoint.y = curr.value;
+                break;
+            case 30:
+                // z value of elevation
+                entity.elevationPoint.z = curr.value;
+                break;
             case 210:
+                // extrusion direction x
                 entity.extrusionDirection.x = curr.value;
                 break;
             case 220:
+                // extrusion direction y
                 entity.extrusionDirection.y = curr.value;
                 break;
             case 230:
+                // extrusion direction z
                 entity.extrusionDirection.z = curr.value;
                 break;
             case 2:
                 entity.hatchPatternName = curr.value;
                 break;
             case 70:
-                entity.solidFillFlag = curr.value;
-                break;
-            case 63:
-                entity.patternFillColor = curr.value;
+                // Solid fill flag (0 = pattern fill; 1 = solid fill); for MPolygon, the version of MPolygon
+                entity.solidFlag = curr.value;
                 break;
             case 71:
+                // Associativity flag (0 = non-associative; 1 = associative); for MPolygon, solid-fill flag (0 = lacks solid fill; 1 = has solid fill)
                 entity.associativityFlag = curr.value;
                 break;
             case 91:
-                entity.numPaths = curr.value;
+                entity.boundaryPaths = new Array(curr.value);
                 break;
-            // case 75:
-            //     // entity.hatchStyle = curr.value;
-            //     // break;
-            // case 76:
-            //     // entity.hatchPatternType = curr.value;
-            //     // break;
-            // case 52:
-            //     // entity.hatchPatternAngle = curr.value;
-            //     // break;
-            // case 41:
-            //     // entity.hatchPatternScale = curr.value;
-            //     // break;
-            // case 47:
-            //     // entity.pixelSize = curr.value;
-            //     // break;
-            // case 98:
-            //     // entity.seedPoints = curr.value;
-            //     // break;
-            // case 11:
-            //     // entity.offsetVector = curr.value;
-            //     // break;
-            // case 99:
-            //     // // this should probably be in boundary path data
-            //     // entity.numDegeneratePaths = curr.value;
-            //     // break;
-            // case 10:
-            //     // entity.seedPoint = helpers.parsePoint(scanner);
-            //     // break;
-            // case 450:
-            //     // entity.solidHatchOrGradient = curr.value;
-            //     // break;
-            // case 451:
-            //     //  -- reserved for future use
-            //     // break;
-            // case 452:
-            //     // entity.howColorsUsed = curr.value;
-            //     // break;
-            // case 453:
-            //     // entity.numberColors = curr.value;
-            //     // break;
-            // case 460:
-            //     // entity.rotationAngle = curr.value;
-            //     // break;
-            // case 461:
-            //     // entity.gradientDefinition = curr.value;
-            //     // break;
-            // case 462:
-            // case 463:
-            // case 470:
-            //     break;
-            default: // ignored attribute
-                if (!helpers.checkCommonEntityProperties(entity, curr)) {
-                    if (curr.code === 92) {
-                        entity.boundaryPathData = {
-                            type: BoundaryPathTypes[curr.value],
-                        }
-                        curr = scanner.next();
-                    } else {
-                        entity.boundaryPathData = parseBoundaryPaths(entity, curr);
-                    }
-                }
+            case 75:
+                // Hatch style:
+                // 0 = Hatch “odd parity” area(Normal style)
+                // 1 = Hatch outermost area only(Outer style)
+                // 2 = Hatch through entire area(Ignore style)
+                entity.hatchStyle = curr.value
+                break;
+            case 76:
+                // Hatch pattern type:
+                // 0 = User-defined
+                // 1 = Predefined
+                // 2 = Custom
+                entity.hatchPatternType = curr.value;
+                break;
+            case 52:
+                // pattern fill only
+                entity.hatchPatternAngle = curr.value; // degrees or radians?
+                break;
+            case 41:
+                // pattern fill only
+                entity.hatchPatternScale = curr.value;
+                break;
+            case 73:
+                entity.boundaryAnnotationFlag = curr.value;
+                break;
+            case 77:
+                entity.hatchPatternDoubleFlag = curr.value;
+                break;
+            case 78:
+                entity.patternDefinitionLines = new Array(curr.value);
+                break;
+            case 47:
+                entity.pixelSize = curr.value;
+                break;
+            case 98:
+                entity.seedPoints = new Array(curr.value);
+                break;
+            case 450:
+                // if code 450 is in the file, then the following codes must be in the file: 451, 452, 453, 460, 461, 462, and 470. If code 450 is not in the file, then the following codes must not be in the file: 451, 452, 453, 460, 461, 462, and 470
+                // 0 = Solid hatch
+                // 1 = Gradient
+                entity.gradient = { isSingleColor: null, numberColors: null, rotationAngle: null, definition: null, tint: null, type: null }
+                break;
+            case 451:
+                // reserved for future use
+                break;
+            case 470:
+                entity.gradient.type = curr.value;
+                break;
+            case 452:
+                entity.gradient.isSingleColor = curr.value;
+                break;
+            case 453:
+                entity.gradient.numberColors = curr.value;
+                break;
+            case 460:
+                entity.gradient.rotationAngle = curr.value; // radians
+                break;
+            case 461:
+                entity.gradient.definition = curr.value;
+                break;
+            case 462:
+                entity.gradient.tint = curr.value;
+                break;
+            case 8:
+                // Always 'A-ANNO-NOTES
+                break;
+            case 1001:
+                // always "ACAD" or "AcDbBlockRepETag" TODO: could this be a start/stop code?
+                break;
+            case 1071:
+                console.log(`Counter? ${curr.value}`);
+                break;
+            default:
+                console.log(`${curr.code} = ${curr.value}`);
                 break;
         }
+        // if (entity.numberOfSeedPoints && entity.numberOfSeedPoints > 0) {
+        //     console.log('End Seed Points')
+        // }
+
         curr = scanner.next();
     }
     console.log(entity)
